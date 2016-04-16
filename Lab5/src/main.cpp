c0434d2d724ccf3e43aed0eddeff5eae24d44c80
SE3910 Student <se3910@cs3841vm>
2016-04-16 13:52:51 -0500

    added lab 5

--- Lab5/src/main.cpp
+++ Lab5/src/main.cpp
@@ -13,17 +13,17 @@
 
 using namespace exploringBB;
 int numCaptures;
-
+Camera* camera = NULL;
 int button1Pressed(int var);
 int button2Pressed(int var);
-Camera camera;
+
 int main(int argc, char*  argv[]){
 	if(argc < 6 || argc > 6){
 		std::cout << "selfie button1 button2 LED width height" << std::endl;
 		exit(0);
 	}
 	numCaptures = 0;
-	camera(0, (int)argv[3], (int)argv[4], (int)argv[5]);
+	camera = new Camera(0, (int)argv[3], (int)argv[4], (int)argv[5]);
 	GPIO* button1 = new GPIO((int)argv[1]); //button 1 png
 	button1->setDirection(GPIO::INPUT);
 	button1->setEdgeType(GPIO::FALLING);
@@ -32,25 +32,25 @@ int main(int argc, char*  argv[]){
 	button2->setDirection(GPIO::INPUT);
 	button2->setEdgeType(GPIO::FALLING);
 	button2->waitForEdge(button2Pressed);
-	camera.start();
+	camera->start();
 	bool running = true;
 	while(running){
 		std::string input;
 		std::cin >> input;
 		if(input == "quit"){
 			std::cout << "shutting camera down" << std::endl;
-			camera.shutdown();
+			camera->shutdown();
 		}
 	}
 
 }
 int button1Pressed(int var){
-	camera.takePicture(0, numCaptures);
+	camera->takePicture(0, numCaptures);
 	numCaptures++;
 	return 0;
 }
 int button2Pressed(int var){
-	camera.takePicture(1, numCaptures);
+	camera->takePicture(1, numCaptures);
 	numCaptures++;
 	return 0;
 }
